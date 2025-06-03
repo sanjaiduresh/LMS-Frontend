13ecdaaacdae9841ab529fa7fa17a9aff33e0e68
sanjaiduresh <sanjaidureshowo@gmai.com>
2025-06-03 17:14:39 +0530

    updated the url

--- src/api.tsx
+++ src/api.tsx
@@ -1,4 +1,4 @@
-// const API_URL = 'https://lms-backend-ofx6.onrender.com';
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://lms-backend-ofx6.onrender.com';
+// const API_URL = 'http://localhost:8000';
 const BULK_UPLOAD_URL = "http://localhost:3000"
 export { BULK_UPLOAD_URL , API_URL };
